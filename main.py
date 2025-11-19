a5d2c5d9b1c099a50b980bf221a34b2c76f0f97b
Kamila <kamilagrzadko@gmail.com>
2025-11-19 14:32:03 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from src.fun_example import add
+
+
+def main() -> None:
+    print(add(11, 10))
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_example.py
@@ -0,0 +1,5 @@
+import pytest
+
+
+def test1() -> None:
+    assert 1 == 1
